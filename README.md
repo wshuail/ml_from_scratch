fda6c026644f7b2b92e70b870906e88268eb51b9
wshuail <oopswangsl@gmail.com>
2020-09-08 17:27:26 +0800

    cart decision tree

--- README.md
+++ README.md
@@ -1,15 +1,5 @@
-# Neural Network from Scratch
+# Machine Learning Algorithms from Scratch
 
-develop neural network from scratch step by step.
+develop machine learning from scratch step by step.
 
-1. Linear Regression  
-2. Linear Regression with Sigmoid Activation  
-3. Linear Regression with Sigmoid Activation Two Layers  
-4. Linear Regression with Tanh Activation  
-5. Linear Regression with ReLU and Sigmoid Activation Two Layers  
-  
-### Inspired by:  
-1.[Project Sophia: Make Neural Networks from Scratch](https://github.com/cstorm125/sophia)  
-2.[Neural Networks Demystified](https://github.com/stephencwelch/Neural-Networks-Demystified)  
-3.[cs231n](http://cs231n.github.io/)  
-4.[nn-from-scratch](https://github.com/dennybritz/nn-from-scratch)  
+1. Decision Tree (CART) 
---
+++ cart/cart.py
@@ -0,0 +1,169 @@
+# CART on the Bank Note dataset
+import random
+from random import seed
+from random import randrange
+from csv import reader
+
+
+# Calculate accuracy percentage
+def accuracy_metric(actual, predicted):
+    correct = 0
+    for i in range(len(actual)):
+        if actual[i] == predicted[i]:
+            correct += 1
+    return correct / float(len(actual)) * 100.0
+
+
+# Split a dataset into k folds
+def cross_validation_split(dataset, n_folds):
+    fold_size = int(len(dataset)/n_folds)
+    dataset = dataset[0: fold_size*n_folds]
+    random.shuffle(dataset)
+    dataset_split = [dataset[i: (i+fold_size)] for i in range(0, len(dataset), fold_size)] 
+    return dataset_split
+
+# Evaluate an algorithm using a cross validation split
+def evaluate_algorithm(dataset, algorithm, n_folds, *args):
+    folds = cross_validation_split(dataset, n_folds)
+    scores = []
+    for i in range(n_folds):
+        test_set = folds[i]
+        train_set = [folds[k] for k in range(len(folds)) if k != i]
+        train_set = [x for split in train_set for x in split]
+        
+        predicted = algorithm(train_set, test_set, *args)
+        actual = [row[-1] for row in folds[i]]
+        accuracy = accuracy_metric(actual, predicted)
+        scores.append(accuracy)
+    return scores
+
+
+# Split a dataset based on an attribute and an attribute value
+def test_split(index, value, dataset):
+    left, right = list(), list()
+    for row in dataset:
+        if row[index] < value:
+            left.append(row)
+        else:
+            right.append(row)
+    return left, right
+
+# Calculate the Gini index for a split dataset
+def gini_index(groups, classes):
+    # count all samples at split point
+    n_instances = float(sum([len(group) for group in groups]))
+    # sum weighted Gini index for each group
+    gini = 0.0
+    for group in groups:
+        size = float(len(group))
+        # avoid divide by zero
+        if size == 0:
+            continue
+        score = 0.0
+        # score the group based on the score for each class
+        for class_val in classes:
+            p = [row[-1] for row in group].count(class_val) / size
+            score += p * p
+        # weight the group score by its relative size
+        gini += (1.0 - score) * (size / n_instances)
+    return gini
+
+# Select the best split point for a dataset
+def get_split(dataset):
+    class_values = list(set(row[-1] for row in dataset))
+    b_index, b_value, b_score, b_groups = 999, 999, 999, None
+    for index in range(len(dataset[0])-1):
+        for row in dataset:
+            groups = test_split(index, row[index], dataset)
+            gini = gini_index(groups, class_values)
+            if gini < b_score:
+                b_index, b_value, b_score, b_groups = index, row[index], gini, groups
+    return {'index':b_index, 'value':b_value, 'groups':b_groups}
+
+# Create a terminal node value
+def to_terminal(group):
+    outcomes = [row[-1] for row in group]
+    return max(set(outcomes), key=outcomes.count)
+
+# Create child splits for a node or make terminal
+def split(node, max_depth, min_size, depth):
+    left, right = node['groups']
+    del(node['groups'])
+    # check for a no split
+    if not left or not right:
+        node['left'] = node['right'] = to_terminal(left + right)
+        return
+    # check for max depth
+    if depth >= max_depth:
+        node['left'], node['right'] = to_terminal(left), to_terminal(right)
+        return
+    # process left child
+    if len(left) <= min_size:
+        node['left'] = to_terminal(left)
+    else:
+        node['left'] = get_split(left)
+        split(node['left'], max_depth, min_size, depth+1)
+    # process right child
+    if len(right) <= min_size:
+        node['right'] = to_terminal(right)
+    else:
+        node['right'] = get_split(right)
+        split(node['right'], max_depth, min_size, depth+1)
+
+# Build a decision tree
+def build_tree(train, max_depth, min_size):
+    root = get_split(train)
+    split(root, max_depth, min_size, 1)
+    return root
+
+# Make a prediction with a decision tree
+def predict(node, row):
+    if row[node['index']] < node['value']:
+        if isinstance(node['left'], dict):
+            return predict(node['left'], row)
+        else:
+            return node['left']
+    else:
+        if isinstance(node['right'], dict):
+            return predict(node['right'], row)
+        else:
+            return node['right']
+
+# Classification and Regression Tree Algorithm
+def decision_tree(train, test, max_depth, min_size):
+    tree = build_tree(train, max_depth, min_size)
+    predictions = []
+    for row in test:
+        prediction = predict(tree, row)
+        predictions.append(prediction)
+    return predictions
+
+# Load a CSV file
+def load_csv(filename):
+    file = open(filename, "rt")
+    lines = reader(file)
+    dataset = list(lines)
+    return dataset
+
+# Convert string column to float
+def str_column_to_float(dataset, column):
+    for row in dataset:
+        row[column] = float(row[column].strip())
+
+
+# Test CART on Bank Note dataset
+seed(2)
+# load and prepare data
+filename = 'data_banknote_authentication.csv'
+dataset = load_csv(filename)
+# convert string attributes to integers
+for i in range(len(dataset[0])):
+    str_column_to_float(dataset, i)
+print ('number of examples: {}'.format(len(dataset)))
+# evaluate algorithm
+n_folds = 5
+max_depth = 5
+min_size = 10
+scores = evaluate_algorithm(dataset, decision_tree, n_folds, max_depth, min_size)
+print('Scores: %s' % scores)
+print('Mean Accuracy: %.3f%%' % (sum(scores)/float(len(scores))))
